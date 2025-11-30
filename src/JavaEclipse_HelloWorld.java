3bc0ac7a6901f17bb749312bb5c6792728d46463
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-11-30 17:02:11 -0500

    Added a 2nd commit

--- src/JavaEclipse_HelloWorld.java
+++ src/JavaEclipse_HelloWorld.java
@@ -4,7 +4,8 @@ public class JavaEclipse_HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World in Java on Eclipse IDE!\n");
+		System.out.println("Hello World in Java on Eclipse IDE!");
+		System.out.println("  2nd print!");
 	}
 
 }

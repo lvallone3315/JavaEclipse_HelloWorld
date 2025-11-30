0c04d9dc3e9b72bf00a0b8b126e443289705733b
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-11-30 16:21:33 -0500

    Initial commit for Java Hello World

---
+++ src/JavaEclipse_HelloWorld.java
@@ -0,0 +1,10 @@
+//   Initial Java program to trial CI/CD pipeline
+
+public class JavaEclipse_HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World in Java on Eclipse IDE!\n");
+	}
+
+}

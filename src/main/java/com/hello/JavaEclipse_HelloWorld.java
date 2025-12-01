da59393d8c6a1fc28e926e8f2043c814c9c3872b
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-12-01 12:29:21 -0500

    class-698 - Add a third print string to test Jenkins automatically runs on a push.

--- src/main/java/com/hello/JavaEclipse_HelloWorld.java
+++ src/main/java/com/hello/JavaEclipse_HelloWorld.java
@@ -8,6 +8,7 @@ public class JavaEclipse_HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println(echo("Hello World in Java on Eclipse IDE!"));
 		System.out.println(echo("  2nd print!"));
+        System.out.println(echo("----------\n\n\n\nBelow the line\n\n-------"));
 	}
 	
 	// adding a method for junit testing
